e77cbeb3c91d5d8be9d1ea530279baa18db0e532
Gerard Braad <me@gbraad.nl>
2023-07-13 07:59:18 +0000

    Just a test

--- src/app.tsx
+++ src/app.tsx
@@ -5,20 +5,43 @@ type ApplicationProps = {
 }
 
 type ApplicationState = {
+    connectionState? : string,
 }
 
 export class Application extends React.Component<ApplicationProps, ApplicationState> {
     state: ApplicationState = {
+        connectionState: null
     }
 
     constructor(props: ApplicationProps) {
         super(props);
+
+        cockpit
+            .spawn(['warp-cli', 'status'])
+            .done(content => {
+                // just a test
+                if(content.includes("Connecting")) {
+                    this.setState(state => ({ connectionState: "Connecting" }));
+                }
+                if(content.includes("Connected")) {
+                    this.setState(state => ({ connectionState: "Connected" }));
+                }
+                if(content.includes("Disconnected")) {
+                    this.setState(state => ({ connectionState: "Disconnected" }));
+                }
+            })
+            .fail(err => {
+
+            });
     }
 
     render() {
         return (
-            <>
-                Hello
+            <>{
+                this.state.connectionState != null
+                    ? <p>{this.state.connectionState}</p>
+                    : <p>Loading...</p>
+                }
             </>
     )}
 }
